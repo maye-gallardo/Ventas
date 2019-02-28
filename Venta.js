344e4001449cf1817fc8e36729147eb78b2ae9a5
MayeGer <maye_gallardo@hotmail.com>
2019-02-28 01:07:50 -0400

    add articule

--- Venta.js
+++ Venta.js
@@ -1,9 +1,23 @@
-class Venta{
-    constructor(){
-        
+class Venta {
+    
+    constructor() {
+        this.articulo = new Articulo();
+        this.quantityArticles = 0;
     }
-    total(){
-        return 0;
+
+    total() {
+        return this.articulo.calculation(this.quantityArticles);
+    }
+
+    add() {
+        this.quantityArticles = this.quantityArticles + 1;
+    }
+}
+
+
+class Articulo{
+    calculation(quantity) {
+        return quantity * 100;
     }
 }
 module.exports = Venta;
--- test/venta_test.js
+++ test/venta_test.js
@@ -1,26 +1,20 @@
+var assert = require('assert');
 var expect = require('chai').expect;
-var should = require('chai').should();
 
-import Venta from'../venta.js'
+import Venta from '../Venta.js';
 
 describe('Venta', function(){
-    it('Total de venta sin producto deberia ser 0',function(){
+
+    it ('Total de venta sin producto deberia ser 0', function(){
         let venta = new Venta();
         expect(venta.total()).equal(0);
     });
 
-    /*it('total de venta con un producto deberia devolver el total del producto',function(){
+    it ('Deberia mostrar el total de un solo articulo de un consto de 100', function(){
         let venta = new Venta();
-        let pantalla = new Articulo("Pantalla SMS 17",100);
-        venta.add(pantalla, 1);
-        expect(venta.total()).equal(100);
+        venta.add();
+        expect(venta.total(1)).equal(100);
     });
 
-    it('total de venta con un servicio deberia devolver el total del producto',function(){
-        let venta = new Venta();
-        let mantenimiento = new Servicio("Pantalla SMS 17",15);
-        venta.add(pantalla,1)
-        venta.add(mantenimiento, 1);
-        expect(venta.total()).equal(115);
-    });*/
+    
 });
